3818bee9e3465cccb4af1b8ff208bcdf41adf31c
DannyTayman <122898168+DannyTayman@users.noreply.github.com>
2024-02-25 22:36:31 -0500

    Add files via upload

--- Stop Light.py
+++ Stop Light.py
@@ -24,17 +24,41 @@ myCanvas.create_oval(50, 200, 150, 300, fill="Black", width = 2)
 myCanvas.create_oval(550, 200, 650, 300, fill="Black", width = 2)
 myCanvas.create_oval(300, 210, 400, 310, fill="Black", width = 2)
 myCanvas.create_oval(300, 590, 400, 690, fill="Black", width = 2)
+myCanvas.create_line(175, 200, 175, 500, arrow="both", fill="Black")
+myCanvas.create_line(525, 200, 525, 500, arrow="both", fill="Black")
+myCanvas.create_line(300, 330, 400, 330, arrow="both", fill="Black")
+myCanvas.create_line(300, 370, 400, 370, arrow="both", fill="Black")
+
+shape1={'bounds': [245, 260, 300, 220, 300, 300], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape1.values())[0],fill='black',outline='black')
+shape2={'bounds': [455, 260, 400, 220, 400, 300], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape2.values())[0],fill='black',outline='Black')
+shape3={'bounds': [245, 650, 300, 610, 300, 690], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape3.values())[0],fill='black',outline='black')
+shape4={'bounds': [455, 650, 400, 610, 400, 690], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape4.values())[0],fill='black',outline='Black')
+
+shape5={'bounds': [50, 200, 150, 200, 100, 135], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape5.values())[0],fill='black',outline='black')
+shape6={'bounds': [50, 500, 150, 500, 100, 565], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape6.values())[0],fill='black',outline='Black')
+shape7={'bounds': [550, 200, 650, 200, 600, 135], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape7.values())[0],fill='black',outline='black')
+shape8={'bounds': [550, 500, 650, 500, 600, 565], 'kind': 'tri', 'fill': True}
+myCanvas.create_polygon(list(shape8.values())[0],fill='black',outline='Black')
 
 a = 10
 
 while True:
-    def green():
+    def ew_green():
         i = 0
         while(i < a):
             green = myCanvas.create_oval(50, 400, 150, 500, fill="Green", width = 2)
             green = myCanvas.create_oval(550, 400, 650, 500, fill="Green", width = 2)
             green = myCanvas.create_oval(300, 10, 400, 110, fill="Red", width = 2)
             green = myCanvas.create_oval(300, 390, 400, 490, fill="Red", width = 2)
+            green = myCanvas.create_line(300, 330, 400, 330, arrow="both", fill="Green")
+            green = myCanvas.create_line(300, 370, 400, 370, arrow="both", fill="Green")
             traffic_lights.update()
             time.sleep(0.5)
             i = i+1
@@ -45,11 +69,13 @@ while True:
             green = myCanvas.create_oval(550, 400, 650, 500, fill="Black", width = 2)
             green = myCanvas.create_oval(300, 10, 400, 110, fill="Black", width = 2)
             green = myCanvas.create_oval(300, 390, 400, 490, fill="Black", width = 2)
+            green = myCanvas.create_line(300, 330, 400, 330, arrow="both", fill="Black")
+            green = myCanvas.create_line(300, 370, 400, 370, arrow="both", fill="Black")
             traffic_lights.update()
             time.sleep(0.000001)
             i = i+1
     
-    def yellow():
+    def ew_yellow():
         i = 0
         while(i < a):
             yellow = myCanvas.create_oval(50, 300, 150, 400, fill = "Yellow", width = 2)
@@ -77,6 +103,8 @@ while True:
             red = myCanvas.create_oval(550, 200, 650, 300, fill = "Red", width = 2)
             red = myCanvas.create_oval(300, 210, 400, 310, fill="Green", width = 2)
             red = myCanvas.create_oval(300, 590, 400, 690, fill="Green", width = 2)
+            red = myCanvas.create_line(175, 200, 175, 500, arrow="both", fill="Green")
+            red = myCanvas.create_line(525, 200, 525, 500, arrow="both", fill="Green")
             traffic_lights.update()
             time.sleep(0.5)
             i=i+1
@@ -87,6 +115,8 @@ while True:
             red = myCanvas.create_oval(550, 200, 650, 300, fill="Black", width = 2)
             red = myCanvas.create_oval(300, 210, 400, 310, fill="Black", width = 2)
             red = myCanvas.create_oval(300, 590, 400, 690, fill="Black", width = 2)
+            red = myCanvas.create_line(175, 200, 175, 500, arrow="both", fill="Black")
+            red = myCanvas.create_line(525, 200, 525, 500, arrow="both", fill="Black")
             traffic_lights.update()
             time.sleep(0.000001)
             i = i+1
@@ -111,14 +141,80 @@ while True:
             traffic_lights.update()
             time.sleep(0.000001)
             i = i+1
+    def ew_l_green_ns_r_green():
+        i = 0
+        while(i<a):
+            green = myCanvas.create_polygon(list(shape1.values())[0],fill='green',outline='black')
+            green = myCanvas.create_polygon(list(shape4.values())[0],fill='green',outline='black')
+            green = myCanvas.create_polygon(list(shape5.values())[0],fill='green',outline='black')
+            green = myCanvas.create_polygon(list(shape8.values())[0],fill='green',outline='black')
+            traffic_lights.update()
+            time.sleep(0.5)
+            i = i+1
+    def ew_r_green_ns_l_green():
+        i = 0
+        while(i<a):
+            green = myCanvas.create_polygon(list(shape2.values())[0],fill='green',outline='black')
+            green = myCanvas.create_polygon(list(shape3.values())[0],fill='green',outline='black')
+            green = myCanvas.create_polygon(list(shape6.values())[0],fill='green',outline='black')
+            green = myCanvas.create_polygon(list(shape7.values())[0],fill='green',outline='black')
+            traffic_lights.update()
+            time.sleep(0.5)
+            i = i+1
+    def ew_l_green_ns_r_greenb():
+        i = 0
+        while(i<2):
+            green = myCanvas.create_polygon(list(shape1.values())[0],fill='black',outline='black')
+            green = myCanvas.create_polygon(list(shape4.values())[0],fill='black',outline='black')
+            green = myCanvas.create_polygon(list(shape5.values())[0],fill='black',outline='black')
+            green = myCanvas.create_polygon(list(shape8.values())[0],fill='black',outline='black')
+            traffic_lights.update()
+            time.sleep(0.00000001)
+            i = i+1
+    def ew_r_green_ns_l_greenb():
+        i = 0
+        while(i<2):
+            green = myCanvas.create_polygon(list(shape2.values())[0],fill='black',outline='black')
+            green = myCanvas.create_polygon(list(shape3.values())[0],fill='black',outline='black')
+            green = myCanvas.create_polygon(list(shape6.values())[0],fill='black',outline='black')
+            green = myCanvas.create_polygon(list(shape7.values())[0],fill='black',outline='black')
+            traffic_lights.update()
+            time.sleep(0.00000001)
+            i = i+1
 
-    green()
+    def ew_yellow2():
+        i = 0
+        while(i < a):
+            yellow = myCanvas.create_oval(50, 300, 150, 400, fill = "Yellow", width = 2)
+            yellow = myCanvas.create_oval(550, 300, 650, 400, fill = "Yellow", width = 2)
+            traffic_lights.update()
+            time.sleep(0.1)
+            i = i+1
+    def ew_yellowb2():
+        i = 0
+        while(i < 2):
+            yellow = myCanvas.create_oval(50, 300, 150, 400, fill="Black", width = 2)
+            yellow = myCanvas.create_oval(550, 300, 650, 400, fill="Black", width = 2)
+            traffic_lights.update()
+            time.sleep(0.000001)
+            i = i+1
+
+    
+    ew_green()
     greenb()
-    yellow()
+    ew_yellow()
     yellowb()
+    ew_r_green_ns_l_green()
+    ew_r_green_ns_l_greenb()
+    ew_yellow2()
+    ew_yellowb2()
     red()
     redb()
     yellow2()
     yellowb2()
+    ew_l_green_ns_r_green()
+    ew_l_green_ns_r_greenb()
+    ew_yellow()
+    yellowb()
 
 traffic_lights.mainloop()
